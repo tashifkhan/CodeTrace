76d5df24412cb0e6903998df646e37da4fab0ed1
Tashif Ahmad Khan <tashifkhan010@gmail.com>
2026-07-05 02:01:07 +0530

    chore: update the prod deployement schema and all

--- vite.config.ts
+++ vite.config.ts
@@ -8,15 +8,40 @@ export default defineConfig({
   server: {
     port: 5173,
     proxy: {
+      '/api/github': {
+        target: 'https://github-stats.tashif.codes',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api\/github/, ''),
+      },
+      '/api/leetcode': {
+        target: 'https://leetcode-stats.tashif.codes',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api\/leetcode/, ''),
+      },
       '/api/codeforces': {
         target: 'https://codeforces-stats.tashif.codes',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api\/codeforces/, ''),
       },
+      '/api/gfg': {
+        target: 'https://gfg-stats.tashif.codes',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api\/gfg/, ''),
+      },
       '/api/codechef': {
-        target: 'https://codechef-stats-api-two.vercel.app',
+        target: 'https://codechef-stats.tashif.codes',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api\/codechef/, ''),
+      },
+      '/api/hackerrank': {
+        target: 'https://hackerrank-stats.tashif.codes',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api\/hackerrank/, ''),
+      },
+      '/api/tuf': {
+        target: 'https://tuf-stats.tashif.codes',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api\/tuf/, ''),
       },
     },
   },
